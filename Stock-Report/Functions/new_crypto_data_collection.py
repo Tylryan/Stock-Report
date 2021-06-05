82c10ae9a1e3a24719e4da33be6dd3f6c31b4ac2
Tyler Ryan <tlrrn5@gmail.com>
2021-06-05 04:41:55 -0500

    Finished with ARIMA for both Stock and Crypto

--- Stock-Report/Functions/new_crypto_data_collection.py
+++ Stock-Report/Functions/new_crypto_data_collection.py
@@ -7,12 +7,6 @@ import matplotlib.pyplot as plt
 import datetime
 pd.set_option('display.width', None)
 
-ticker = 'BTC'
-
-
-start = datetime.date(2019, 12, 2)
-end = datetime.date(2021, 6, 2)
-interval = '1HRS'
 
 # Example intervals
 # SEC, MIN, HRS, DAY
@@ -23,7 +17,7 @@ def get_close_data(
         start: datetime,
         end: datetime,
         api_key: str,
-        interval: str = '1HRS'
+        interval: str = '1DAY'
 ):
 
     # Date range of the request
@@ -45,6 +39,7 @@ def get_close_data(
     df = df[['Date', 'Close']]
     # Only Show Year Month Day
     df['Date'] = pd.to_datetime(df['Date'].str[:10])
+    df.set_index('Date', inplace=True)
     return df
 
 
@@ -60,6 +55,10 @@ if __name__ == '__main__':
     env_location = '../../Data/.env'
     user_name, password, crypto_api = read_config.export_variables(
         env_location)
+    ticker = 'BTC'
+
+    end = datetime.date.today()
+    start = end - datetime.timedelta(days=505)
     df = get_close_data(ticker, start, end, crypto_api)
     # df.set_index('Date', inplace=True)
     # df['Short'] = df.Close.rolling(window=24).mean()
--- Stock-Report/main.py
+++ Stock-Report/main.py
@@ -1,29 +1,65 @@
 #!/usr/bin/python3
 
+# adding Folder_2 to the system path
+import sys
+sys.path.insert(
+    0, '/home/tyler/Documents/Coding/Stock-Report/Stock-Report/Functions')
 # External Libraries
-import yfinance as yf
-import pandas as pd
-from time import sleep
+
 
 # Personal Modules
-from Functions.data_analysis import stock_report
-from Functions.email_user import send_mail
-import Functions.stock_sources
-from Functions.read_config import export_variables
-tickers = ['AAPL', 'GOOGL', 'PLUG']
-
-
-username, password = export_variables()
-report = stock_report(tickers)
+stock_ticker = 'AAPL'
+crypto_ticker = 'BTC'
 
 
 def main():
-    report = stock_report(tickers)
-    send_mail(
-        from_email=username,
-        to_emails=username,
-        report=report
-    )
+    from read_config import export_variables
+    # from email_user import send_mail
+    from data_analysis import stock_report
+    from new_crypto_data_collection import get_close_data
+    from arma_test import arima_prediction
+    import os
+    from time import sleep
+    import pandas as pd
+    import yfinance as yf
+    import stock_sources
+    import read_config
+    import datetime
+    print('End')
+
+    ####################### Stock Data ######################################
+    stock_df = stock_sources.long_period_df(stock_ticker)
+    stock_close_df = stock_sources.only_close(stock_df)
+
+    print(stock_close_df)
+
+    ####################### Crypto Data ####################################
+
+    env_location = '../Data/.env'
+    user_name, password, crypto_api = read_config.export_variables(
+        env_location)
+    ticker = 'BTC'
+
+    end = datetime.date.today()
+    # 2 Years according to stock data
+    start = end - datetime.timedelta(days=505)
+    crypto_close_df = get_close_data(ticker, start, end, crypto_api)
+    print(crypto_close_df)
+    ####################### Predictions #####################################
+
+    stock_prediction_mse, stock_prediction = arima_prediction(stock_close_df)
+    print(f"Stock Prediction MSE {stock_prediction_mse}")
+    # crypto_prediction_mse, crypto_prediction = arima_prediction(
+    #     crypto_close_df)
+    # username, password = export_variables()
+    # report = stock_report(tickers)
+
+    # report = stock_report(tickers)
+    # send_mail(
+    #     from_email=username,
+    #     to_emails=username,
+    #     report=report
+    # )
 
 
 if __name__ == "__main__":
